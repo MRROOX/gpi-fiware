bc7c55cecb87ea5ad5822f3eb5d012622170ea9e
Felipe Quezada <f.quezada01@ufromail.com>
2019-09-22 14:59:38 -0300

    Correcion error con parentecis

--- IOT/FIWARE-TEST/dht22-fiware+conf.py
+++ IOT/FIWARE-TEST/dht22-fiware+conf.py
@@ -21,9 +21,9 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity)
     
-        payloadTem = "temdht22|{0:0.1f}".format(temperature, humidity))
+        payloadTem = "temdht22|{0:0.1f}".format(temperature, humidity)
   
         responseTem = requests.request("POST", url, data=payloadTem, headers=iot_conf["headers"], params=iot_conf["querystring"])
 
