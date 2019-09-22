d1f68e2dec3ed84742e9f60b9ed47877200ff54e
Felipe Quezada <f.quezada01@ufromail.com>
2019-09-22 14:54:07 -0300

    DHT22 ejemplo + configuración

--- IOT/FIWARE-TEST/dht22-fiware+conf.py
+++ IOT/FIWARE-TEST/dht22-fiware+conf.py
@@ -1,6 +1,13 @@
+# import os
+# import time
+# import Adafruit_DHT
+
 import requests
 import json
 from pprint import pprint
+
+# DHT_SENSOR = Adafruit_DHT.DHT22
+# DHT_PIN = 4
 
 json_data_file = open("./config/iot-conf.json", "r").read() # r for reading the file
 iot_conf = json.loads(json_data_file)
@@ -9,8 +16,28 @@ pprint(iot_conf)
 
 url = "http://"+iot_conf["host_r"]+":"+iot_conf["port_r"]+iot_conf["remote_r"]
 print(url)
-payload = "temdht22|2305"
 
-response = requests.request("POST", url, data=payload, headers=iot_conf["headers"], params=iot_conf["querystring"])
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+    
+        payloadTem = "temdht22|{0:0.1f}".format(temperature)
+  
 
-print(response.text)
+        responseTem = requests.request("POST", url, data=payloadTem, headers=iot_conf["headers"], params=iot_conf["querystring"])
+
+        print(responseTem.text)
+
+        payloadHum = "humdht22|{1:0.1f}".format(humidity)
+
+        responseHum = requests.request("POST", url, data=payloadHum, headers=iot_conf["headers"], params=iot_conf["querystring"])
+
+        print(responseHum.text)
+
+    else:
+        print("Failed to retrieve data from humidity sensor")
+    time.sleep(iot_conf["time_sleep"])
+   
+
