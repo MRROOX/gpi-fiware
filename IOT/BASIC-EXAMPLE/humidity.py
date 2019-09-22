e4ccaead99d154a556c307b5f999dcf248e3515c
Felipe Quezada <f.quezada01@ufromail.com>
2019-09-22 15:18:38 -0300

    up

--- IOT/BASIC-EXAMPLE/humidity.py
+++ IOT/BASIC-EXAMPLE/humidity.py
@@ -8,7 +8,7 @@ while True:
 
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-        Temperatura = "{0:0.1f}".format(temperture, humidity)
+        Temperatura = "{0:0.1f}".format(temperature, humidity)
         print(Temperatura)
     else:
         print("Failed to retrieve data from humidity sensor")
