12c8a38e636a358bac3055d30259d34942cfe7f5
Felipe Quezada <f.quezada01@ufromail.com>
2019-09-22 15:34:55 -0300

    Catch  KeyboardInterrupt

--- IOT/FIWARE-TEST/dht22-fiware+conf.py
+++ IOT/FIWARE-TEST/dht22-fiware+conf.py
@@ -42,3 +42,13 @@ while True:
     time.sleep(iot_conf["time_sleep"])
    
 
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
